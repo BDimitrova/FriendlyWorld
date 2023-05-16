bfc8913ef9eed872dce2cd90bf77adb456d93600
Boryana Dimitrova <b.dimitrova97@gmail.com>
2023-05-16 14:49:54 +0300

    Setup login, register and logout

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path');
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 
-// const { auth } = require('../middlewares/authMiddleware');
+const { auth } = require('../middlewares/authMiddleware');
 
 function expressConfig(app) {
     app.locals.title = 'Friendly World'
     app.use('/static', express.static(path.join(__dirname, '../public')));
-    // app.use(express.urlencoded({ extended: true }));
-    // app.use(cookieParser());
-    // app.use(auth);
+    app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser());
+    app.use(auth);
 }
 
 module.exports = expressConfig; 
