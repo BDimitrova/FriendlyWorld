0e6f1938eb4133afb90f86cf588e8ede1295aef0
Boryana Dimitrova <b.dimitrova97@gmail.com>
2023-05-16 10:53:03 +0300

    Add animals controller and login, register page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 const { PORT } = require('./constants');
-const router = require('./router');
+const routers = require('./router');
 
 require('./config/hbsConfig')(app);
 require('./config/expressConfig')(app);
 
-app.use(router);
+app.use(routers);
 
 app.listen(PORT, () => console.log(`The app is running on port ${PORT}...`))
--- src/router.js
+++ src/router.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const homeController = require('./controller/homeController');
+const authController = require('./controller/authController');
+const animalsController = require('./controller/animalsController');
 
-router.use(homeController);
+router.use('/', homeController);
+router.use('/auth', authController);
+router.use('/animals', animalsController);
 
 module.exports = router
