1fad25cb23c1e126decdc32aec554c4bf3786bc3
Boryana Dimitrova <b.dimitrova97@gmail.com>
2023-05-16 11:33:40 +0300

    Install and setup mongoose

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ const app = express();
 const { PORT } = require('./constants');
 const routers = require('./router');
 
+const initDatabase = require('./config/mongooseConfig');
+
 require('./config/hbsConfig')(app);
 require('./config/expressConfig')(app);
 
 app.use(routers);
 
-app.listen(PORT, () => console.log(`The app is running on port ${PORT}...`))
+initDatabase()
+    .then(() => {
+        app.listen(PORT, () => console.log(`The app is running on http://localhost:${PORT}`));
+    })
+    .catch((err) => {
+        console.log('Cannot connect database:', err);
+    })
